404eacbdbc961f7b98b877f900caa66d8c92e32f
Robert Gregan <robertgregan@Roberts-MacBook-Air.local>
2017-04-19 18:52:37 -0300

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import * as firebase from 'firebase';
+
+// should go in a secret file
+const config = {
+apiKey: "AIzaSyCXwQfuc_RHn-crZTWnHfmJMUypYUwibBM",
+    authDomain: "chatapp-78e75.firebaseapp.com",
+    databaseURL: "https://chatapp-78e75.firebaseio.com",
+    projectId: "chatapp-78e75",
+    storageBucket: "chatapp-78e75.appspot.com",
+    messagingSenderId: "301688987192"
+  };
+
+  firebase.initializeApp(config); 
+
+   export default firebase;
